dd700b81e1aaac9f8abccc3ad1b11e6ae68d2de9
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    input: hardware buttons lights integration
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    * Addresses NOUGAT-14
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -483,6 +483,18 @@ public final class PowerManager {
 
     /**
      * Gets the maximum supported screen brightness setting for VR Mode.
+     * Gets the minimum supported button brightness setting.
+     * The screen may be allowed to become dimmer than this value but
+     * this is the minimum value that can be set by the user.
+     * @hide
+     */
+    public int getMinimumButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingMinimum);
+    }
+
+    /**
+     * Gets the maximum supported button brightness setting.
      * The screen may be allowed to become dimmer than this value but
      * this is the maximum value that can be set by the user.
      * @hide
@@ -494,6 +506,26 @@ public final class PowerManager {
 
     /**
      * Gets the default screen brightness for VR setting.
+     * @hide
+     */
+
+    public int getMaximumButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingMaximum);
+    }
+
+    /**
+     * Gets the default button brightness setting.
+     * @hide
+     */
+    public int getDefaultButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingDefault);
+    }    
+
+    /**
+     * Returns true if the twilight service should be used to adjust screen brightness
+     * policy.  This setting is experimental and disabled by default.
      * @hide
      */
     public int getDefaultScreenBrightnessForVrSetting() {
